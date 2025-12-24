668f3d14a240ae21c6752f34efea7d41993535c5
Soban Ali <sobanali19@yahoo.com>
2025-12-23 23:34:50 -0500

    Learned about Iframes, starting to looad puzzle in

--- src/main/java/TestSetup.java
+++ src/main/java/TestSetup.java
@@ -1,5 +1,10 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TestSetup {
@@ -15,14 +20,17 @@ public class TestSetup {
             WebDriver driver = new ChromeDriver();
             System.out.println("✓ Chrome opened");
             
-            System.out.println("Step 3: Navigating to Google...");
-            driver.get("https://www.google.com");
+            System.out.println("Step 3: Navigating to LinkedIn Tango...");
+            driver.get("https://www.linkedin.com/games/tango");
+			Thread.sleep(5000);
+			Thread.sleep(2000);
+			System.out.println("TESTING FOR FINDING ELEMENT");
+			WebElement startButton = driver.findElement(By.id("launch"));
+startButton.click();
             System.out.println("✓ Navigation complete");
-            
-            System.out.println("Step 4: Getting page title...");
-            String title = driver.getTitle();
-            System.out.println("Page title: " + title);
-            
+			
+			System.out.println("CLICKING START");
+			// startButton.get(0).click();
             System.out.println("Step 5: Closing browser...");
             driver.quit();
             System.out.println("✓ Browser closed");
